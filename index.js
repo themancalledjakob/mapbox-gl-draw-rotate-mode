30eff5e0442afcf55b66f059feb9432857c8c9d9
asukhorukova1 <asukhorukova@inbox.ru>
2019-06-11 14:34:26 +0200

    add a touch

--- index.js
+++ index.js
@@ -44,8 +44,91 @@ var RotateMode = {
       return state;
     },
 
+    onTouchStart: function(state, e) {
+      if(e.featureTarget) {
+        if(this._ctx.api.get(e.featureTarget.properties.id)) {
+          e.target['dragPan'].disable();
+          state.selectedFeature = this._ctx.api.get(e.featureTarget.properties.id);
+          state.originalCenter = centroid(e.featureTarget);
+          state.originalFeature = e.featureTarget;
+          emitter.emit('rotatestart');
+        }
+      }
+      return state;
+    },
+
     toDisplayFeatures: function(state, geojson, display) {
       display(geojson);
+    },
+
+    onTouchMoveDrag: function(state, e) {
+      if(state.selectedFeature&&state.mode) {
+        if(state.mode==='rotate') {
+          state.lastMouseDownLngLat = {lng:e.lngLat.lng, lat: e.lngLat.lat};
+          var draggedBearing = bearing(state.originalCenter, [e.lngLat.lng, e.lngLat.lat]);
+          var rotatedCoords = [];
+          switch (state.originalFeature.properties['meta:type']) {
+            case 'Point':
+              break;
+            case 'LineString':
+              state.originalFeature.geometry.coordinates.forEach(function(coords,index) {
+                var distanceFromCenter = distance(state.originalCenter, coords);
+                var bearingFromCenter = bearing(state.originalCenter, coords);
+                var newPoint = destination(state.originalCenter, distanceFromCenter, bearingFromCenter+draggedBearing);
+                rotatedCoords.push(newPoint.geometry.coordinates);
+              })
+              break;
+            case 'Polygon':
+              var polyCoords = [];
+              state.originalFeature.geometry.coordinates[0].forEach(function(coords,index) {
+                var distanceFromCenter = distance(state.originalCenter, coords);
+                var bearingFromCenter = bearing(state.originalCenter, coords);
+                var newPoint = destination(state.originalCenter, distanceFromCenter, bearingFromCenter+draggedBearing);
+                polyCoords.push(newPoint.geometry.coordinates);
+              })
+              rotatedCoords.push(polyCoords);
+              break;
+            case 'MultiLineString':
+              var multipolys = [];
+              state.originalFeature.geometry.coordinates.forEach(function(polygon,index) {
+                var polyCoords = [];
+                polygon.forEach(function(coords,index) {
+                  var distanceFromCenter = distance(state.originalCenter, coords);
+                  var bearingFromCenter = bearing(state.originalCenter, coords);
+                  var newPoint = destination(state.originalCenter, distanceFromCenter, bearingFromCenter+draggedBearing);
+                  polyCoords.push(newPoint.geometry.coordinates);
+                })
+                multipolys.push(polyCoords);
+              })
+              rotatedCoords = multipolys;
+              break;
+            case 'MultiPolygon':
+              var multipolys = [];
+              state.originalFeature.geometry.coordinates.forEach(function(polygon,index) {
+                var polyCoords = [];
+                polygon.forEach(function(polygonHoles,index) {
+                  var polyHoleCoords = [];
+                  polygonHoles.forEach(function(coords,index) {
+                    var distanceFromCenter = distance(state.originalCenter, coords);
+                    var bearingFromCenter = bearing(state.originalCenter, coords);
+                    var newPoint = destination(state.originalCenter, distanceFromCenter, bearingFromCenter+draggedBearing);
+                    polyHoleCoords.push(newPoint.geometry.coordinates);
+                  });
+                  polyCoords.push(polyHoleCoords);
+                })
+                multipolys.push(polyCoords);
+              })
+              rotatedCoords = multipolys;
+              break;
+            default:
+              return;
+          }
+          emitter.emit('rotating');
+          var newFeature = state.selectedFeature;
+          newFeature.geometry.coordinates = rotatedCoords;
+          var thisFeat = this._ctx.api.add(newFeature);
+        }
+      }
     },
 
     onDrag: function(state, e) {
@@ -116,6 +199,15 @@ var RotateMode = {
           var thisFeat = this._ctx.api.add(newFeature);
         }
       }
+    },
+
+    onTouchEnd: function(state, e) {
+      e.target['dragPan'].enable();
+      emitter.emit('rotateend');
+      state.selectedFeature = false;
+      state.lastMouseDownLngLat = false;
+      state.originalCenter = false;
+      return state;
     },
 
     onMouseUp: function(state, e) {
